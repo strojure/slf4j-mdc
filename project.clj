77c26c3ba4645d2175977526dc56af4df0046e89
Sergey Trofimov <sergey@trofimov.name>
2023-03-07 15:23:06 +0100

    (chore) Change license to MIT, same as `slf4j`

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject com.github.strojure/slf4j-mdc "1.1.1-SNAPSHOT"
   :description "Clojure utility for SLF4J logging system's MDC (Mapped Diagnostic Context)."
   :url "https://github.com/strojure/slf4j-mdc"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]
                  [org.slf4j/slf4j-api "1.7.36"]]
